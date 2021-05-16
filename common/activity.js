dd5e1c6260119569de025ba3cf9d9fceffbfae01
Doug Waldron <douglas.waldron@dnr.ga.gov>
2021-05-15 22:15:12 -0400

    Fix bug in distance display

--- common/activity.js
+++ common/activity.js
@@ -6,10 +6,29 @@ import { units } from "user-settings";
 import * as datetime from "../common/datetime";
 
 function formatNumber(num) {
-  if (num < 0.01) return "0";
-  if (num < 1) return num.toPrecision(2);
   if (num < 1000) return num.toString();
   return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+}
+
+function formatDistance(num) {
+  if (units.distance == "metric") {
+    // meters
+    if (num < 1000) return `${num} m`;
+
+    // meters to km
+    num = parseFloat((num / 1000).toPrecision(3));
+    return `${formatNumber(num)} km`;
+  } else {
+    if (num < 1610) {
+      // meters to ft
+      num = parseFloat((num * 3.28084).toPrecision(3));
+      return `${formatNumber(num)} ft`;
+    }
+
+    // meters to miles
+    num = parseFloat((num / 1609.344).toPrecision(3));
+    return `${formatNumber(num)} mi`;
+  }
 }
 
 let activitySpacingHorizontal = 80;
@@ -32,32 +51,19 @@ if (today.local.elevationGain === undefined) {
 
 const primaryGoalIndex = (function () {
   for (var i = 0; i < activityTypes.length; i++) {
-    if (primaryGoal == activityTypes[i]) {
-      return i;
-    }
+    if (primaryGoal == activityTypes[i]) return i;
   }
 })();
 
 let showPrimaryClockFace = true;
 
-function drawActivity(el, activityType, showValue) {
+function drawActivity(el, activityType, showValue, isPrimary) {
   let actual = today.adjusted[activityType] || 0;
   let goal = goals[activityType] || 1;
-  let distanceUnits = "";
 
   if (activityType == "activeZoneMinutes") {
     actual = today.adjusted[activityType].total || 0;
     goal = goals[activityType].total || 1;
-  } else if (activityType == "distance") {
-    if (units.distance == "metric") {
-      actual = (actual / 1000).toPrecision(3); // meters to kilometers
-      goal = (goal / 1000).toPrecision(3); // meters to kilometers
-      distanceUnits = " km";
-    } else {
-      actual = (actual / 1609.344).toPrecision(3); // meters to miles
-      goal = (goal / 1609.344).toPrecision(3); // meters to miles
-      distanceUnits = " mi";
-    }
   }
 
   const progressEl = el.getElementsByClassName("progress")[0];
@@ -68,22 +74,24 @@ function drawActivity(el, activityType, showValue) {
     progressEl.sweepAngle = 360;
   } else {
     completedEl.style.display = "none";
-    progressEl.sweepAngle = 360 * (actual / goal);
+    // Use 350 instead of 360 to make sure gap in ring is
+    // visible even when very close to goal.
+    progressEl.sweepAngle = 350 * (actual / goal);
   }
 
   if (showValue) {
     const valueEl = el.getElementsByClassName("value")[0];
 
     if (valueEl !== undefined) {
-      valueEl.text = `${formatNumber(actual)}${distanceUnits}`;
-
-      if (actual > 99999) {
-        valueEl.style.fontSize = 60;
-        valueEl.x = 90;
+      if (activityType == "distance") {
+        valueEl.text = formatDistance(actual);
+      } else {
+        valueEl.text = formatNumber(actual);
       }
 
-      if (activityType == "distance") {
+      if (isPrimary && (actual > 99999 || activityType == "distance")) {
         valueEl.style.fontSize = 60;
+        valueEl.x = 90;
       }
     }
   }
@@ -104,7 +112,7 @@ function drawPrimaryClockFace() {
   const iconEl = el.getElementsByClassName("icon")[0];
   iconEl.href = `icons/${primaryActivity}_48.png`;
 
-  drawActivity(el, primaryActivity, true);
+  drawActivity(el, primaryActivity, true, true);
 
   // Draw other activities horizontally across bottom
   let posX = 0;
@@ -124,7 +132,7 @@ function drawPrimaryClockFace() {
       el.x = posX;
       el.y = 0;
       posX += activitySpacingHorizontal;
-      drawActivity(el, activityType, false);
+      drawActivity(el, activityType, false, false);
     }
   }
 }
@@ -147,7 +155,7 @@ function drawSecondaryClockFace() {
     el.x = 0;
     el.y = posY;
     posY += activitySpacingVertical;
-    drawActivity(el, activityType, true);
+    drawActivity(el, activityType, true, false);
   }
 }
 
